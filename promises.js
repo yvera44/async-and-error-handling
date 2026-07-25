ccda7f4d8d1228708432fcb55c9fc5257bceab7b
YairVera <yairvera44@gmail.com>
2026-07-25 12:45:15 -0500

    create custome promise

--- promises.js
+++ promises.js
@@ -1,6 +1,8 @@
 //Import Node Filesystem MOdule (the promises version)
-
 import fs from "node:fs/promises";
+
+//Import Node Filesytem module
+import fsc from "node:fs";
 
 // Reading the file with the promises API
 fs.readFile('./data.json', 'utf8')
@@ -9,7 +11,21 @@ fs.readFile('./data.json', 'utf8')
         console.log(dataObj);
         console.log("Complete");
     })
+    .then(() => readFile("data.json"))
+    .then(data => console.log(data))
     .catch(err => {
         console.log('Could not complete loading and parsing.');
         throw err;
     });
+
+//Create a custom Promise with the callback API
+const readFile = async (filename) => {
+    return new Promise((resolve, reject) => {
+        fsc.readFile('data.json', 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(data);
+        });
+    });
+}
